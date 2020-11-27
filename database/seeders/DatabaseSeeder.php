de1f1d34c276de71564eb9cfc4f50e03d971d7f5
Eni <estefanielunardi@users.noreply.github.com>
2020-11-27 11:07:12 +0100

    git init

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \App\Models\User::factory(1)->create(
+            [   
+                'name'=> 'Cannoli',
+                'isAdmin'=>true
+            ]
+        );
+        
+        \App\Models\User::factory(10)->create();
+    }
+}

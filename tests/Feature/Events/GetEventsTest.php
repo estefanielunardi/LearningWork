f80abc40e9d8f1e0abdd9d6caad46d7f1cb7c695
Carmenprz <carmenp.gomez99@gmail.com>
2020-12-01 09:46:48 +0100

    test get events

--- tests/Feature/Events/GetEventsTest.php
+++ tests/Feature/Events/GetEventsTest.php
@@ -14,36 +14,16 @@ class GetEventsTest extends TestCase
 
     public function test_can_retrive_all_events()
     {
-        //crear los eventos recogidos en una tabla en la bbdd
         $events= Event::factory(4)->create(); 
+        
         $this->assertCount(4, $events); 
-        //buscar la tabla con eventos en la bbdd
-        $events= Event::all();
-        
-        //comprobar  routar controller
-        
-        //comprobar la ruta 
 
+        $events= Event::all();
 
         $response = $this->get(route('comingEvents'))
             ->assertStatus(200)
             ->assertViewIs('comingEvents')
             ->assertViewHas('events', $events)
             ->assertSee($events[0] -> name);
-
-
-
-        // $response = $this->get(route('welcome'))
-        //     ->assertStatus(200); 
-
-            //comprobar que la vista es la correcta
-            // ->assertViewIs('comingEvents'); 
-        
-        // //comprobar vista tenga todos los eventos
-        // ->assertViewHas('comingEvents, $events');
-        // //todos los eventos se deben mostrar
-
-        
-
     }
 }
